5ad136ff93b4e91d22a86af9ba684504b2b7d0ae
Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>
2026-06-28 19:20:46 +0530

    Refactor entrypoint.sh for improved configuration handling
    Signed-off-by: Mukesh Kharb <mukesh.Kharb.snaatak@mygurukulam.co>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 CONFIG_FILE=${CONFIG_FILE:-"/app/config.yaml"}
 
 if [ ! -f "${CONFIG_FILE}" ]; then
+
 cat <<EOF > "${CONFIG_FILE}"
 ---
 server:
@@ -16,23 +17,25 @@ smtp:
   username: "${SMTP_USERNAME}"
   password: "${SMTP_PASSWORD}"
   smtp_server: "${SMTP_SERVER}"
-  smtp_port: "${SMTP_PORT}"
+  smtp_port: ${SMTP_PORT}
 
 elasticsearch:
   username: "${ELASTIC_USERNAME}"
   password: "${ELASTIC_PASSWORD}"
   host: "${ELASTIC_HOST}"
-  port: "${ELASTIC_PORT}"
+  port: ${ELASTIC_PORT}
+  index: "${ELASTIC_INDEX:-employee_index}"
 EOF
+
 fi
 
 export CONFIG_FILE=${CONFIG_FILE}
 
-echo "========================================"
+echo "===================================================="
 echo "Starting Notification API"
 echo "Configuration : ${CONFIG_FILE}"
 echo "Port          : 8085"
-echo "========================================"
+echo "===================================================="
 
 exec gunicorn \
     --bind 0.0.0.0:8085 \
